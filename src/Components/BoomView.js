c998aa0780b4237f5035a0e09d65a7ea94904105
Imran Peerbhai <imran@thinkpredict.com>
2019-03-30 14:12:54 -0700

    Suzanne the monkey now shows on localhost:3000/static/monkey.html.  How to make it smarter?

--- src/Components/BoomView.js
+++ src/Components/BoomView.js
@@ -6,11 +6,7 @@ import { Entity, Scene } from 'aframe-react';
 const BoomView = (props) => {
     return (
         <Scene>
-            <Entity geometry={{ primitive: 'box' }} material={{ color: 'red' }} position={{ x: 0, y: 0, z: -5 }} />
-            <Entity particle-system={{ preset: 'snow' }} />
-            <Entity light={{ type: 'point' }} />
-            <Entity gltf-model={{ src: 'virtualcity.gltf' }} />
-            <Entity text={{ value: 'Hello, WebVR!' }} />
+            <Entity obj-model="obj: url('/static/img/monkey.obj')"></Entity>
         </Scene>
     );
 }
