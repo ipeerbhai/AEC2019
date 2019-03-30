a9610f41d2a0a320c279255b24febddbf3005706
Imran Peerbhai <imran@thinkpredict.com>
2019-03-30 13:21:16 -0700

    Some sort of hello world renders.

--- src/Components/BoomView.js
+++ src/Components/BoomView.js
@@ -1,8 +1,18 @@
 import React from 'react';
-
+import 'aframe';
+import 'aframe-particle-system-component';
+import { Entity, Scene } from 'aframe-react';
 
 const BoomView = (props) => {
-    return (<div>Hello, world</div>);
+    return (
+        <Scene>
+            <Entity geometry={{ primitive: 'box' }} material={{ color: 'red' }} position={{ x: 0, y: 0, z: -5 }} />
+            <Entity particle-system={{ preset: 'snow' }} />
+            <Entity light={{ type: 'point' }} />
+            <Entity gltf-model={{ src: 'virtualcity.gltf' }} />
+            <Entity text={{ value: 'Hello, WebVR!' }} />
+        </Scene>
+    );
 }
 
 export default BoomView;
